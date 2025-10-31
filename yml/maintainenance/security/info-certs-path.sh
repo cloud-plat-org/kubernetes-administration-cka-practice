77efe2a2ce4dbf6bf67c77486e6135eb8cd1dfd5
Dan <daniverson@hotmail.com>
2025-10-31 14:42:08 -0400

    start api and apis II

--- yml/maintainenance/security/info-certs-path.sh
+++ yml/maintainenance/security/info-certs-path.sh
@@ -348,21 +348,4 @@ source ~/.bashrc
 # now kubectl will use the custom kubeconfig file
 kubectl get pods
 
-curl -k https://development:6443/version
-curl -k https://development:6443/api/v1/namespaces/default/pods
-/metrics # health check
-/healthz # health check
-/api # api server (core groups) *
-/apis # api server (named groups) *
-/logs # logs for third party integrations
-
-
-
-
-
-
-
-
-
-
 
