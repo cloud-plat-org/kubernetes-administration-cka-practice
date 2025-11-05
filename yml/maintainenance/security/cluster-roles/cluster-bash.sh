02308fdc99966744d0e7b4d90db0e962bda375ad
Dan <daniverson@hotmail.com>
2025-11-05 11:51:53 -0500

    finished lab 1

--- yml/maintainenance/security/cluster-roles/cluster-bash.sh
+++ yml/maintainenance/security/cluster-roles/cluster-bash.sh
@@ -27,6 +27,31 @@ kubectl api-resources --namespaced=true
 
 kubectl create -f cluster-admin-role.yml
 
+kubectl create -f cluster-admin-rolebinding.yml 
+
+# Cluser roles can be used for namespace scoped resources as well.
+
+ k get clusterroles --no-headers | wc -l
+
+ k get clusterrolebindings --no-headers | wc -l
+
+k create clusterrole view-nodes --resource=nodes --verb=get,list,watch
+k describe clusterrole view-nodes
+
+k create clusterrolebinding view-nodes-binding --clusterrole=view-nodes --user=michelle
+k describe clusterrolebinding view-nodes-binding
+
+kubectl api-resources --namespaced=false
+
+PersistentVolume
+StorageClass
+VolumeAttachment
+VolumeAttributesClass
+k create clusterrole storage-admin --resource=storageclasses,persistentvolumes --verb=get,list,watch,create,update,delete
+k describe clusterrole storage-admin
+
+k create clusterrolebinding michelle-storage-admin --clusterrole=storage-admin --user=michelle
+k describe clusterrolebinding michelle-storage-admin
 
 
 
