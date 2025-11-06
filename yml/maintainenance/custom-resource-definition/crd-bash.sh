ad5eb21cde8ec23f926a40828258ce9d8fe130cc
Dan <daniverson@hotmail.com>
2025-11-06 16:00:39 -0500

    CRD

---
+++ yml/maintainenance/custom-resource-definition/crd-bash.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Deployments
+kubectl create -f deployment.yml
+kubectl get deployments
+kubectl delete -f deployment.yml
+# This all edit the etcd database.
+# what creates the deployment is the Deployment controller.
+
+# Controller
+# monitors the etcd database for changes to the deployment resource.
+# The monitor is an application written in go, it is part of kubernetes cluster.
+# There are different controllers for different resources.
+
+#  https://kubernetes.io/docs/concepts/extend-kubernetes/api-extension/custom-resources/
+
+# Custom Resource Definition (CRD)
+# What if I want to create a resource like flight ticket?
+kubectl create -f flight-ticket.yml
+kubectl get flighttickets
+kubectl delete -f flight-ticket.yml
+# This all edit the etcd database.
+
+# If I try to run create it will fail because the resource is not defined (No controller for flight ticket)
+# This will not work because the api server does not know how to handle the flight ticket resource.
+# We need to define the resource in the api server.
+# We can define the resource in the api server by creating a Custom Resource Definition (CRD)
+
+kubeclt api-resources
+# name, shortNames, namespaced, kind, verbs, shortNames, categories, storageVersion
+# name: flightticket
+# shortNames: ft
+# namespaced: true
+# kind: FlightTicket
+# verbs: create, delete, get, list, patch, update, watch
+# shortNames: ft
+# categories: flights.com
+# storageVersion: v1
+
+kubectl create -f flightticket-custom-definition.yml
+kubectl get crds
+# Now you can create the flight ticket object.
+kubectl create -f flight-ticket.yml
+kubectl get flighttickets
+kubectl delete -f flight-ticket.yml
+# This all edit the etcd database.
+
+# Sill needs a contoller to watch the etcd database for changes to the flight ticket object.
+
+
+
+
