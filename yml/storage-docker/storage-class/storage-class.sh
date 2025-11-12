006bff3fdb42190158f505c8c00089bfea166cc1
Dan <daniverson@hotmail.com>
2025-11-12 15:32:11 -0500

    sc definitions

--- yml/storage-docker/storage-class/storage-class.sh
+++ yml/storage-docker/storage-class/storage-class.sh
@@ -14,3 +14,36 @@
 ## Static Provisioning ##
 
 # This is where storage classes are used to provision the storage.
+
+kubectl create -f sc-definition.yml
+kubectl create -f pvc-sc-definition.yml
+kubectl create -f pod-definition.yml
+
+kubectl get pv
+kubectl get pvc
+kubectl get pod
+
+kubectl describe pod random-number-generator
+kubectl describe pvc myclaim
+kubectl describe sc google-storage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
